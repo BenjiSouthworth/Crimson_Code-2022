091acf2b2ef9d68647ea7d4160293e1598cd1b72
hung-luu <h.luu1998@gmail.com>
2022-02-19 18:07:31 -0800

    + Added initial app layout

---
+++ co_cougs/client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div class="header-container">
+            <div class="header-content">CoCoug - Passing Knowledge to Underclassmen</div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ co_cougs/client/src/components/SideMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideMenu.css'
+
+const SideMenu = () => {
+    return (
+        <div class="sidemenu-container">
+            <div class="header-content">Side Menu</div>
+        </div>
+    )
+}
+
+export default SideMenu;
